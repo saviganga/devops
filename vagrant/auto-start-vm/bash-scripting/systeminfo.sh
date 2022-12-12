7f53b81932869586bf71c0865f1753fa6c3731ca
saviganga <akatugbasaviour@gmail.com>
2022-12-12 10:08:35 +0100

    intro to bash scripting

---
+++ vagrant/auto-start-vm/bash-scripting/systeminfo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# who am i
+echo "who am i"
+whoami
+echo
+
+echo "ip address info"
+ip addr show
+echo
+
+echo "hostname info"
+cat /etc/hosts
+echo
+
+echo "uptime info"
+uptime
+echo
